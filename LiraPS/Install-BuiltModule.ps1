8788f270f1fbfe3298b551d1a333d3788edd2837
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2025-10-04 22:28:54 +0200

    Added publish and install script

---
+++ LiraPS/Install-BuiltModule.ps1
@@ -0,0 +1,24 @@
+#Requires -Version 7
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $ModuleName = (Split-Path $PSScriptRoot -Leaf),
+    [Parameter()]
+    [string]
+    $ProjectName = (Split-Path $PSScriptRoot -Leaf)
+)
+$info = . $PSScriptRoot/Publish.ps1 -Module $ModuleName -Project $ProjectName
+$paths = $env:PSModulePath -split ';'
+foreach ($path in $paths) {
+    try {
+        $installedModulePath = Join-Path $path $ModuleName
+        New-Item -ItemType Directory -Path $installedModulePath -Force -ea Stop
+        Copy-Item -Path $info.Path -Destination $installedModulePath -Recurse -Verbose
+        Write-Host 'Installed moduled in $path'
+        return
+    } catch {
+        <#Do this if a terminating exception happens#>
+    }
+}
+Write-Error -ea Stop 'Could not install module'
---
+++ LiraPS/Publish.ps1
@@ -0,0 +1,26 @@
+#Requires -Version 7
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $ModuleName = (Split-Path $PSScriptRoot -Leaf),
+    [Parameter()]
+    [string]
+    $ProjectName = (Split-Path $PSScriptRoot -Leaf)
+)
+Push-Location $PSScriptRoot
+$version = ([xml](Get-Content "$ProjectName.csproj")).Project.PropertyGroup.Version ?? '0.0.1'
+$versionedFolder = "$PSScriptRoot/published/$version"
+New-Item -ItemType Directory -Path $versionedFolder -Force
+Write-Host "Building with version $version"
+dotnet publish -o $versionedFolder -f net8.0 -c Release --self-contained
+if ($LASTEXITCODE) {
+    Write-Error -ea stop 'COULD NOT BUILD'
+}
+$modulePath = "$versionedFolder/$ModuleName.psd1"
+$newLines = Get-Content $modulePath | ForEach-Object { $_ -replace '^\s*ModuleVersion\s?=.*', "ModuleVersion = '$Version'" } 
+$newLines | Set-Content $modulePath
+[PSCustomObject]@{
+    Path    = $versionedFolder
+    Version = $version
+}
