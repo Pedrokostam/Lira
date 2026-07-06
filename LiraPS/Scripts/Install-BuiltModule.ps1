ade9e134a89d30fc4af375d417cf11a9e798fb36
Maciej Krosta <Maciej.Krosta@zebra.com>
2026-07-06 09:44:25 +0200

    fixed publishing scripts; need to improve remove-worklogs

--- LiraPS/Scripts/Install-BuiltModule.ps1
+++ LiraPS/Scripts/Install-BuiltModule.ps1
@@ -1,24 +1,28 @@
 #Requires -Version 7
 [CmdletBinding()]
 param (
-    [Parameter()]
-    [string]
-    $ModuleName = (Split-Path $PSScriptRoot -Leaf),
-    [Parameter()]
-    [string]
-    $ProjectName = (Split-Path $PSScriptRoot -Leaf)
+   [Parameter()]
+   [string]
+   $ModuleName = (Split-Path (Split-Path $PSScriptRoot) -Leaf),
+   [Parameter()]
+   [string]
+   $ProjectName = (Split-Path (Split-Path $PSScriptRoot) -Leaf)
 )
 $info = . $PSScriptRoot/Publish.ps1 -Module $ModuleName -Project $ProjectName
 $paths = $env:PSModulePath -split ';'
-foreach ($path in $paths) {
-    try {
-        $installedModulePath = Join-Path $path $ModuleName
-        New-Item -ItemType Directory -Path $installedModulePath -Force -ea Stop
-        Copy-Item -Path $info.Path -Destination $installedModulePath -Recurse -Verbose -force -ea stop
-        Write-Host "Installed moduled in $path"
-        return
-    } catch {
-        <#Do this if a terminating exception happens#>
-    }
+foreach ($path in $paths)
+{
+   try
+   {
+      $installedModulePath = Join-Path $path $ModuleName
+      New-Item -ItemType Directory -Path $installedModulePath -Force -ea Stop
+      Copy-Item -Path $info.Path -Destination $installedModulePath -Recurse -Verbose -Force -ea stop
+      Write-Host "Installed moduled in $path"
+      return
+   }
+   catch
+   {
+      <#Do this if a terminating exception happens#>
+   }
 }
 Write-Error -ea Stop 'Could not install module'
--- LiraPS/Scripts/Publish.ps1
+++ LiraPS/Scripts/Publish.ps1
@@ -1,12 +1,12 @@
 #Requires -Version 7
 [CmdletBinding()]
 param (
-    [Parameter()]
-    [string]
-    $ModuleName = (Split-Path (Split-Path $PSScriptRoot) -Leaf),
-    [Parameter()]
-    [string]
-    $ProjectName = (Split-Path (Split-Path $PSScriptRoot) -Leaf)
+   [Parameter()]
+   [string]
+   $ModuleName = (Split-Path (Split-Path $PSScriptRoot) -Leaf),
+   [Parameter()]
+   [string]
+   $ProjectName = (Split-Path (Split-Path $PSScriptRoot) -Leaf)
 )
 Push-Location (Split-Path $PSScriptRoot)
 $csprojXmlProperties = ([xml](Get-Content "$ProjectName.csproj")).Project.PropertyGroup
@@ -16,72 +16,83 @@ New-Item -ItemType Directory -Path $versionedFolder -Force
 Write-Host "Building $ProjectName with version $version"
 dotnet publish -o $versionedFolder -f net8.0 -c Release --self-contained
 
-$docDir = Measure-PlatyPSMarkdown -Path .\Docs\*.md | ? FileType -Match "CommandHelp"  |
-Import-MarkdownCommandHelp -Path {$_.FilePath} | Export-MamlCommandHelp -output $versionedFolder -force
-Rename-Item $docDir.Directory -NewName "en-US"
+$docDir = Measure-PlatyPSMarkdown -Path .\Docs\*.md | Where-Object FileType -Match "CommandHelp"  |
+   Import-MarkdownCommandHelp -Path { $_.FilePath } | Export-MamlCommandHelp -output $versionedFolder -Force
+$outputDocFolder = "$versionedFolder/en-US" 
+$interimDocFolder = "$versionedFolder/$ProjectName" 
+if (Test-Path $outputDocFolder)
+{
+   Remove-Item -Recurse -Force $outputDocFolder 
+}
+Rename-Item $interimDocFolder -NewName "en-US" -Force
 
-write-host  "$versionedFolder/$ProjectName - $versionedFolder/en-US"
+Write-Host  "$versionedFolder/$ProjectName - $versionedFolder/en-US"
 
-if ($LASTEXITCODE) {
-    Write-Error -ea stop 'COULD NOT BUILD'
+if ($LASTEXITCODE)
+{
+   Write-Error -ea stop 'COULD NOT BUILD'
 }
 $modulePath = "$versionedFolder/$ModuleName.psd1"
 $newLines = Get-Content $modulePath | ForEach-Object { $_ -replace '^\s*ModuleVersion\s?=.*', "ModuleVersion = '$Version'" } 
 $newLines | Set-Content $modulePath
 $nugetPresent = Get-Command nuget -ea SilentlyContinue
-if ($nugetPresent) {
-    Push-Location 'published'
-    $nuspecPath = 'LastNuspec.nuspec'
-    '' > $nuspecPath
-    $nuspecPath = Get-Item $nuspecPath | ForEach-Object fullname
-    $xmlsettings = New-Object System.Xml.XmlWriterSettings
-    $xmlsettings.Indent = $true
-    $xmlWriter = [System.Xml.XmlWriter]::Create($nuspecPath, $xmlsettings)
-    $xmlWriter.WriteStartElement('package')
+if ($nugetPresent)
+{
+   Push-Location 'published'
+   $nuspecPath = 'LastNuspec.nuspec'
+   '' > $nuspecPath
+   $nuspecPath = Get-Item $nuspecPath | ForEach-Object fullname
+   $xmlsettings = New-Object System.Xml.XmlWriterSettings
+   $xmlsettings.Indent = $true
+   $xmlWriter = [System.Xml.XmlWriter]::Create($nuspecPath, $xmlsettings)
+   $xmlWriter.WriteStartElement('package')
 
-    $xmlWriter.WriteStartElement('metadata')
-    $xmlWriter.WriteElementString('id', $ModuleName)
-    $xmlWriter.WriteElementString('version', $version)
+   $xmlWriter.WriteStartElement('metadata')
+   $xmlWriter.WriteElementString('id', $ModuleName)
+   $xmlWriter.WriteElementString('version', $version)
 
-    $copyPasteElement = @('title', 'authors', 'owners', 'licenseUrl', 'projectUrl', 'iconUrl', 'requireLicenseAcceptance', 'description', 'summary', 'releaseNotes', 'copyright', 'language', 'tags')
-    foreach ($elem in $copyPasteElement) {
-        $val = $csprojXmlProperties."$Elem"
-        if ($val) {
-            $xmlWriter.WriteElementString($elem, $val)
-        }
-    }
-    $repoUrl = $csprojXmlProperties.RepositoryUrl
-    $repoType = $csprojXmlProperties.RepositoryType
-    if ($repoUrl -and $repoType) {
-        $xmlWriter.WriteStartElement('repository')
-        $xmlWriter.WriteAttributeString('type', $repoType)
-        $xmlWriter.WriteAttributeString('url', $repoUrl)
-        $xmlWriter.WriteEndElement()
-    }
-    $xmlWriter.WriteEndElement()
+   $copyPasteElement = @('title', 'authors', 'owners', 'licenseUrl', 'projectUrl', 'iconUrl', 'requireLicenseAcceptance', 'description', 'summary', 'releaseNotes', 'copyright', 'language', 'tags')
+   foreach ($elem in $copyPasteElement)
+   {
+      $val = $csprojXmlProperties."$Elem"
+      if ($val)
+      {
+         $xmlWriter.WriteElementString($elem, $val)
+      }
+   }
+   $repoUrl = $csprojXmlProperties.RepositoryUrl
+   $repoType = $csprojXmlProperties.RepositoryType
+   if ($repoUrl -and $repoType)
+   {
+      $xmlWriter.WriteStartElement('repository')
+      $xmlWriter.WriteAttributeString('type', $repoType)
+      $xmlWriter.WriteAttributeString('url', $repoUrl)
+      $xmlWriter.WriteEndElement()
+   }
+   $xmlWriter.WriteEndElement()
 
-    $xmlWriter.WriteStartElement('files')
+   $xmlWriter.WriteStartElement('files')
 
-    $xmlWriter.WriteStartElement('file')
-    $src = (Join-Path $versionedFolder '**') -replace '\\', '/'
-    $xmlWriter.WriteAttributeString('src', $src)
-    $xmlWriter.WriteAttributeString('target', "content/$ModuleName")
-    $xmlWriter.WriteEndElement()
+   $xmlWriter.WriteStartElement('file')
+   $src = (Join-Path $versionedFolder '**') -replace '\\', '/'
+   $xmlWriter.WriteAttributeString('src', $src)
+   $xmlWriter.WriteAttributeString('target', "content/$ModuleName")
+   $xmlWriter.WriteEndElement()
 
-    $xmlWriter.WriteEndElement()
-    $xmlWriter.WriteEndElement()
+   $xmlWriter.WriteEndElement()
+   $xmlWriter.WriteEndElement()
 
-    $xmlWriter.Flush()
-    $xmlWriter.Close()
+   $xmlWriter.Flush()
+   $xmlWriter.Close()
 
-    nuget pack $nuspecPath
-    Get-Content $nuspecPath
-    Pop-Location 
+   nuget pack $nuspecPath
+   Get-Content $nuspecPath
+   Pop-Location 
 }
 
 Pop-Location
 [PSCustomObject]@{
-    Path    = $versionedFolder
-    Version = $version
-    Nuget   = if ($nugetPresent) { "$ModuleName.$version.nupkg" } else { $null }
+   Path    = $versionedFolder
+   Version = $version
+   Nuget   = if ($nugetPresent) { "$ModuleName.$version.nupkg" } else { $null }
 }
